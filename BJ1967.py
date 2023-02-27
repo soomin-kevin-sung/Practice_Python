cf0b170b1ca974cc0e2e4625e778202286525584
kevin0725 <ssm0725@gmail.com>
2023-02-27 13:54:54 +0900

    푼거 올림

--- BJ1967.py
+++ BJ1967.py
@@ -1,53 +1,41 @@
 import sys
+from collections import deque
 
+tree = []
 n = 0
-edges = []
+
+
+def get_most_far_node_with_dist(start):
+    q = deque([start])
+    dist = [-1] * (n + 1)
+    dist[start] = 0
+
+    while len(q) > 0:
+        node = q.popleft()
+
+        for next_node in tree[node]:
+            if dist[next_node] == -1:
+                dist[next_node] = dist[node] + tree[node][next_node]
+                q.append(next_node)
+
+    return max([(i, dist[i]) for i in range(1, n + 1)], key=lambda t: t[1])
 
 
 def main(input=sys.stdin, output=sys.stdout):
-    global n, edges
+    global tree, n
 
     n = int(input.readline())
+    tree = [{} for _ in range(n + 1)]
 
-    edges = [{} for _ in range(n + 1)]
-    for i in range(n - 1):
-        a, b, v = map(int, input.readline().split())
-        edges[a][b] = v
-        edges[b][a] = v
+    for _ in range(n - 1):
+        a, b, c = map(int, input.readline().split())
+        tree[a][b] = c
+        tree[b][a] = c
 
-    leaf = []
-    for i in range(1, n + 1):
-        if len(edges[i]) == 1:
-            leaf.append(i)
+    start, _ = get_most_far_node_with_dist(1)
+    end, ans = get_most_far_node_with_dist(start)
 
-    if len(leaf) == 1:
-        output.write(f'{get_lowest_cost(1, leaf[0])}')
-    else:
-        ans = []
-        for node in leaf:
-            ans.append((node, get_lowest_cost(1, node)))
-
-        ans.sort(key=lambda t: t[1], reverse=True)
-
-        output.write(f'{get_lowest_cost(ans[0][0], ans[1][0])}')
-
-
-def get_lowest_cost(start, end):
-    global n, edges
-
-    lowest_cost = float('inf')
-    for node in edges[start]:
-        if node == end:
-            lowest_cost = min(lowest_cost, edges[start][node])
-        else:
-            v = edges[node][start]
-            del edges[node][start]
-
-            lowest_cost = min(lowest_cost, edges[start][node] + get_lowest_cost(node, end))
-
-            edges[node][start] = v
-
-    return lowest_cost
+    output.write(f'{ans}')
 
 
 if __name__ == '__main__':
