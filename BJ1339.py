08f9e621a0defdc969f27654c4626e2f82b4c3f3
kevin0725 <ssm0725@gmail.com>
2023-03-23 10:14:14 +0900

    bj1339 품

--- BJ1339.py
+++ BJ1339.py
@@ -3,53 +3,21 @@ import sys
 input = sys.stdin
 output = sys.stdout
 
-
-def get_char_number_dict(words):
-    digit_count = {}
-
-    for word in words:
-        for i in range(len(word)):
-            c = word[i]
-
-            if c not in digit_count:
-                digit_count[c] = [0, 0]
-
-            digit_count[c][0] = max(digit_count[c][0], len(word) - i)
-            digit_count[c][1] += 1
-
-    tuples = []
-    for c in digit_count:
-        data = digit_count[c]
-        tuples.append((c, data[0], data[1]))
-
-    tuples.sort(key=lambda t: (t[1], t[2], t[0]), reverse=True)
-
-    key_number = {}
-
-    for i in range(len(tuples)):
-        key_number[tuples[i][0]] = 9 - i
-
-    return key_number
-
-
-# input n.
 n = int(input.readline())
-words = []
+cnt = [0] * 26
 
-# input words.
 for _ in range(n):
-    words.append(input.readline().strip())
+    word = input.readline().strip()
+    for i in range(len(word)):
+        cnt[ord(word[i]) - 65] += 10 ** (len(word) - i - 1)
 
-# get dict [char, number].
-char_number_dict = get_char_number_dict(words)
+cnt.sort(reverse=True)
 
-# calculate.
 ans = 0
-for word in words:
-    d = 10 ** (len(word) - 1)
-    for c in word:
-        ans += char_number_dict[c] * d
-        d //= 10
+num = 9
+for i in range(26):
+    if cnt[i]:
+        ans += cnt[i] * num
+        num -= 1
 
-# output ans.
 output.write(f'{ans}')
