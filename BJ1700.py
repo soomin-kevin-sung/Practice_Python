50f0343ad33778b6bdd71ace31628e5f00b0e03c
KevinSung <ssm0725@gmail.com>
2023-03-25 21:50:30 +0900

    bj1700 품

--- BJ1700.py
+++ BJ1700.py
@@ -1,13 +1,11 @@
 import sys
 from collections import deque
 
-# 푸는 중 (20%에서 틀림)
-
 input = sys.stdin
 output = sys.stdout
 
 
-def get_release_concent(levels):
+def get_release_concent(used, levels):
     last_obj = 0
     last_level = 0
     for used_obj in used:
@@ -16,37 +14,42 @@ def get_release_concent(levels):
                 last_level = levels[used_obj][0]
                 last_obj = used_obj
         else:
-            last_level = 101
             last_obj = used_obj
+            break
 
     return last_obj
+
+
+def get_release_count(order):
+    k = len(order)
+    levels = [deque() for _ in range(k + 1)]
+
+    for i in range(k):
+        levels[order[i]].append(i)
+
+    cnt = 0
+
+    used = set()
+    for obj in order:
+        if obj not in used:
+            if len(used) < n:
+                used.add(obj)
+            else:
+                rel_obj = get_release_concent(used, levels)
+                used.remove(rel_obj)
+                used.add(obj)
+
+                cnt += 1
+
+        if levels[obj]:
+            levels[obj].popleft()
+
+    return cnt
 
 
 n, k = map(int, input.readline().split())
 order = list(map(int, input.readline().split()))
 
-levels = [deque() for _ in range(k + 1)]
+ans = get_release_count(order)
 
-for i in range(k):
-    levels[order[i]].append(i)
-
-cnt = 0
-used = set()
-
-for level in range(k):
-    obj = order[level]
-
-    if obj in used:
-        continue
-    elif len(used) < n:
-        used.add(obj)
-        if levels[obj]:
-            levels[obj].popleft()
-    else:
-        rel_obj = get_release_concent(levels)
-        used.remove(rel_obj)
-        used.add(obj)
-
-        cnt += 1
-
-output.write(f'{cnt}')
+output.write(f'{ans}')
