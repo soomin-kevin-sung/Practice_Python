503e2ebd6322ba0ecfa94492892f583631e361f0
KevinSung <ssm0725@gmail.com>
2023-03-26 19:25:53 +0900

    bj3109 푸는 중

---
+++ BJ3109.py
@@ -0,0 +1,23 @@
+import sys
+
+# 푸는 중
+
+input = sys.stdin
+output = sys.stdout
+
+def exist_way_to_bakery(table, start):
+    dr = [-1, 0, 1]
+    dc = [1, 1, 1]
+
+
+
+    return True
+
+r, c = map(int, input.readline().split())
+
+table = []
+for _ in range(r):
+    table.append(list(input.readline().strip()))
+
+for i in range(r):
+    pass
