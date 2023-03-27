2e3431cf3c7c360acd8ec24f225293946e2302cd
kevin0725 <ssm0725@gmail.com>
2023-03-27 12:11:15 +0900

    bj3109 품

--- BJ3109.py
+++ BJ3109.py
@@ -8,20 +8,16 @@ table = []
 dr = [-1, 0, 1]
 
 
-def is_valid_pos(row, col):
-    return 0 <= row < r and 0 <= col < c
-
-
 def exist_way_to_bakery(nr, nc):
     table[nr][nc] = 'm'
 
     if nc == c - 1:
         return True
     else:
-        for i in range(3):
-            tr = nr + dr[i]
+        for d in dr:
+            tr = nr + d
             tc = nc + 1
-            if is_valid_pos(tr, tc) and table[tr][tc] == '.':
+            if 0 <= tr < r and 0 <= tc < c and table[tr][tc] == '.':
                 if exist_way_to_bakery(tr, tc):
                     return True
 
@@ -29,13 +25,9 @@ def exist_way_to_bakery(nr, nc):
 
 
 if __name__ == '__main__':
-    # r, c = map(int, input.readline().split())
-    # for _ in range(r):
-    #     table.append(list(input.readline().strip()))
-
-    r = 10000
-    c = 500
-    table = [['.' for _ in range(c)] for _ in range(r)]
+    r, c = map(int, input.readline().split())
+    for _ in range(r):
+        table.append(list(input.readline().strip()))
 
     ans = 0
     for i in range(r):
