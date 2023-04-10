01004975462700479d47a3db6e64d68b04c88f8d
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-04-10 16:27:01 +0900

    [unrated] Title: 과제 진행하기, Time: 124.99 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
@@ -1,48 +1,13 @@
-import datetime as dt
-
-
 def solution(plans):
-    studies = parsing(plans)
-    ans = []
-    st = []
+    plans = sorted(map(lambda x: [x[0], int(x[1][:2]) * 60 + int(x[1][3:]), int(x[2])], plans), key=lambda x: x[1])
 
-    prev_subject, prev_starttime, prev_taketime = studies[0]
-    prev_end = prev_starttime + prev_taketime
-
-    for current in studies[1:]:
-        if prev_end > current[1]:
-            st.append([prev_subject, prev_taketime - (current[1] - prev_starttime)])
-        else:
-            ans.append(prev_subject)
-            left = current[1] - prev_end
-            while st:
-                if st[-1][1] > left:
-                    st[-1][1] -= left
-                    break
-                else:
-                    left -= st[-1][1]
-                    ans.append(st[-1][0])
-                    st.pop()
-
-        prev_subject, prev_starttime, prev_taketime = current
-        prev_end = prev_starttime + prev_taketime
-
-    ans.append(prev_subject)
-    while st:
-        ans.append(st.pop()[0])
-
-    return ans
-
-
-def parsing(plans):
-    datas = []
+    lst = []
     for plan in plans:
-        subject = plan[0]
-        start_time = dt.datetime.strptime(plan[1], '%H:%M')
-        start_time = 60 * start_time.hour + start_time.minute
+        for i in range(len(lst)):
+            if lst[i][0] > plan[1]:
+                lst[i][0] += plan[2]
 
-        taken_time = int(plan[2])
-        datas.append([subject, start_time, taken_time])
+        lst.append([plan[1] + plan[2], plan[0]])
 
-    datas.sort(key=lambda t: t[1])
-    return datas
+    lst.sort()
+    return list(map(lambda x: x[1], lst))
