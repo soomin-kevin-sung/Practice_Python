4c7d28c4934f4540055badf30260d916f5ac62dc
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-04-23 21:59:38 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 340 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,10 @@
+def solution():
+    a, b, c, d, e, f = map(int, input().split())
+    for x in range(-999, 1000, 1):
+        for y in range(-999, 1000, 1):
+            if a * x + b * y == c and d * x + e * y == f:
+                print(x, y)
+                return
+
+
+solution()
