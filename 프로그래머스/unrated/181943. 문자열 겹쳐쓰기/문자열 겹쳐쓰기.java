3a6eb52faf627494907944560a7453380e77eb8e
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 02:16:25 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 9.85 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,20 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        StringBuilder builder = new StringBuilder();
-        
-        int idx = 0;
-        var my_chrs = my_string.toCharArray();
-        while (idx < s)
-            builder.append(my_chrs[idx++]);
-        
-        var overwrite_chrs = overwrite_string.toCharArray();
-        for (var c : overwrite_chrs)
-            builder.append(c);
-        
-        idx = s + overwrite_chrs.length;
-        while (idx < my_chrs.length)
-            builder.append(my_chrs[idx++]);
-        
-        return builder.toString();
+        var before = my_string.substring(0, s);
+        var after = my_string.substring(s + overwrite_string.length());
+        return before + overwrite_string + after;
     }
 }
