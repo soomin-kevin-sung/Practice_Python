99553e6304c95cff17e204a315e9389d1dc37ebe
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-02 17:11:09 +0900

    [unrated] Title: a와 b 출력하기, Time: 203.08 ms, Memory: 62.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        var a = sc.nextInt();
+        var b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
