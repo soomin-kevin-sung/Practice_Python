acb463cae2db0de986a88bb320e4b4960279eedf
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-02 17:07:30 +0900

    [unrated] Title: 문자열 출력하기, Time: 170.41 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        var a = sc.next();
+        System.out.println(a);
+    }
+}
