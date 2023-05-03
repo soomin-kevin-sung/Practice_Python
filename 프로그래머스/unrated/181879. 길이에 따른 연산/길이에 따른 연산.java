50de09ac89f47003483ad2f38e08e1f64e68b278
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 11:35:41 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.92 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -1,10 +1,10 @@
-import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int[] num_list) {
         if (num_list.length > 10)
-            return Arrays.stream(num_list).reduce(0, Integer::sum);
+            return IntStream.of(num_list).sum();
         else
-            return Arrays.stream(num_list).reduce(1, (x,y) -> x * y);
+            return IntStream.of(num_list).reduce(1, (x,y) -> x * y);
     }
 }
