8c4809e0a493e23d502d0d5349c4506ae24bae92
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 17:40:56 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        for (int i = 0; i < 3; i++)
+        {
+            if (date1[i] != date2[i])
+                return date1[i] < date2[i] ? 1 : 0;
+        }
+        
+        return 0;
+    }
+}
