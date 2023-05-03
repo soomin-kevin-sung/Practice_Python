9b89ad073b0c066842decef32b7b232aa4aa9c83
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-04 03:28:34 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,18 +1,6 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        int len_myString = my_string.length();
-        int len_suffix = is_suffix.length();
-        
-        if (len_myString >= len_suffix) {
-            for (int i = 1; i <= len_suffix; i++)
-            {
-                if (my_string.charAt(len_myString - i) != is_suffix.charAt(len_suffix - i))
-                    return 0;
-            }
-            
-            return 1;
-        }
-            
-        return 0;
+        int idx = my_string.lastIndexOf(is_suffix);
+        return idx >= 0 && idx + is_suffix.length() == my_string.length() ? 1 : 0;
     }
 }
