801cc4e74a9430462cbe17d2a51120c4b81970ba
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 13:10:14 +0900

    [unrated] Title: 카운트 다운, Time: 3.00 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -2,6 +2,6 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int start, int end) {
-        return IntStream.iterate(start, i -> i - 1).limit(start - end + 1).toArray();
+        return IntStream.rangeClosed(-start, -end).map(i -> -i).toArray();
     }
 }
