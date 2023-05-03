826a32bec0662cb3212f0bf91d51c29a159f2951
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-04 03:10:16 +0900

    [unrated] Title: A 강조하기, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,15 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        int len = myString.length();
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < len; i++) {
-            char c = Character.toLowerCase(myString.charAt(i));
-            if (c == 'a')
-                builder.append('A');
-            else
-                builder.append(c);
-        }
-        
-        return builder.toString();
+        myString = myString.toLowerCase();
+        return myString.replace("a", "A");
     }
 }
