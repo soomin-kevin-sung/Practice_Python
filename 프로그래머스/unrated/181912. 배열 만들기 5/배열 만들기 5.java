6cb7c1812e11cf792b4fc1685f7704f4ca768f7e
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 19:28:13 +0900

    [unrated] Title: 배열 만들기 5, Time: 4.55 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        ArrayList<Integer> list = new ArrayList();
+        for (String str : intStrs) {
+            Integer v = Integer.parseInt(str.substring(s, s + l));
+            if (v > k)
+                list.add(v);
+        }
+            
+        return toArray(list);
+    }
+    
+    private int[] toArray(ArrayList<Integer> list) {
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++)
+            result[i] = list.get(i).intValue();
+        
+        return result;
+    }
+}
