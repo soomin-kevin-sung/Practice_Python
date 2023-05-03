385c4d9060e6fb99664b51d33d58e3ce8e4eaf0d
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 15:55:50 +0900

    [unrated] Title: 조건 문자열, Time: 3.03 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
@@ -1,12 +1,15 @@
+import java.util.Map;
+import java.util.function.BiFunction;
+
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        if (ineq.equals(">") && eq.equals("="))
-            return n >= m ? 1 : 0;
-        else if (ineq.equals("<") && eq.equals("="))
-            return n <= m ? 1 : 0;
-        else if (ineq.equals(">"))
-            return n > m ? 1 : 0;
-        else
-            return n < m ? 1 : 0;
+        Map<String, BiFunction<Integer, Integer, Boolean>> map = Map.of(
+            ">=", (a, b) -> a >= b,
+            "<=", (a, b) -> a <= b,
+            ">!", (a, b) -> a > b,
+            "<!", (a, b) -> a < b
+        );
+        
+        return map.get(ineq + eq).apply(n, m) ? 1 : 0;
     }
 }
