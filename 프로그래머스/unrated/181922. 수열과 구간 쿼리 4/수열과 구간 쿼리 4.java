6f335bec818ca3d60a4959eef82bc100c46b0eda
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 17:44:02 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 1.65 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = Arrays.copyOf(arr, arr.length);
+        
+        for (int[] query : queries) {
+            for (int i = query[0]; i <= query[1]; i++) {
+                if (i % query[2] == 0)
+                    answer[i]++;
+            }
+        }
+        
+        return answer;
+    }
+}
