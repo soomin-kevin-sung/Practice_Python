08d9e1223f3a4d67c86230227981277d20255f1d
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 11:39:18 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,7 +1,7 @@
-import java.util.stream.*;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        return IntStream.of(num_list).limit(n).toArray();
+        return Arrays.copyOfRange(num_list, 0, n);
     }
 }
