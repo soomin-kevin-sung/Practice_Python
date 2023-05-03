aa83387b288d989dd8d423010a2919a9b6aec6e8
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 18:57:37 +0900

    [unrated] Title: 세 개의 구분자, Time: 41.56 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,26 +1,14 @@
 import java.util.*;
 class Solution {
     public String[] solution(String myStr) {
-        ArrayList<String> list = new ArrayList<String>(Arrays.asList(myStr.split("a")));
-        list = getSplitStrings(list, "b");
-        list = getSplitStrings(list, "c");
-        
-        if (list.size() == 0)
-            return new String[] { "EMPTY" };
-        else
-            return list.toArray(new String[0]);
-    }
-    
-    private ArrayList<String> getSplitStrings(ArrayList<String> strings, String splitter) {
         ArrayList<String> list = new ArrayList();
-        for (String s : strings) {
-            for (String pkt : s.split(splitter))
-            {
-                if (!pkt.isEmpty())
-                    list.add(pkt);
-            }
+        String[] pkts = myStr.split("[abc]+");
+        for (String pkt : pkts)
+        {
+            if (!pkt.isEmpty())
+                list.add(pkt);
         }
         
-        return list;
+        return list.size() == 0 ? new String[] { "EMPTY" } : list.toArray(new String[0]);
     }
 }
