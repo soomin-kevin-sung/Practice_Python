728e4edd32132ffc55bc53428c3b45f0b3a60575
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-03 09:35:12 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
@@ -2,44 +2,50 @@ import java.util.*;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        countScore(map, a);
-        countScore(map, b);
-        countScore(map, c);
-        countScore(map, d);
-        
-        int[] keys = map.keySet().stream().mapToInt(Integer::intValue).toArray();
-        switch (keys.length) {
-            case 1:
-                return 1111 * a;
-                
-            case 2:
-                if (map.get(keys[0]) == 3)
-                    return (int)Math.pow(10 * keys[0] + keys[1], 2);
-                else if (map.get(keys[1]) == 3)
-                    return (int)Math.pow(10 * keys[1] + keys[0], 2);
-                else
-                    return (keys[0] + keys[1]) * Math.abs(keys[0] - keys[1]);
-                    
-            case 3:
-                if (map.get(keys[0]) == 2)
-                    return keys[1] * keys[2];
-                else if (map.get(keys[1]) == 2)
-                    return keys[0] * keys[2];
-                else
-                    return keys[0] * keys[1];
-                    
-                
-            case 4:
-                return Math.min(a, Math.min(b, Math.min(c, d)));
-        }
-        
-        return -1;
-    }
-    
-    private void countScore(Map<Integer, Integer> map, Integer key) {
-        Integer old = map.getOrDefault(key, Integer.valueOf(0));
-        
-        map.put(key, old + 1);
+        // a == b == c == d
+        if (a == b && b == c && c == d)
+            return 1111 * a;
+        // a == b == c | d
+        else if (a == b && b == c)
+            return (int)Math.pow(10 * a + d, 2);
+        // a == b == d | c
+        else if (a == b && b == d)
+            return (int)Math.pow(10 * a + c, 2);
+        // a == c == d | b
+        else if (a == c && c == d)
+            return (int)Math.pow(10 * a + b, 2);
+        // b == c == d | a
+        else if (b == c && c == d)
+            return (int)Math.pow(10 * b + a, 2);
+        // a == b | c == d
+        else if (a == b && c == d)
+            return (a + c) * Math.abs(a - c);
+        // a == c | b == d
+        else if (a == c && b == d)
+            return (a + b) * Math.abs(a - b);
+        // a == d | b == c
+        else if (a == d && b == c)
+            return (a + b) * Math.abs(a - b);
+        // a == b | c | d
+        else if (a == b)
+            return c * d;
+        // a == c | b | d
+        else if (a == c)
+            return b * d;
+        // a == d | b | c
+        else if (a == d)
+            return b * c;
+        // b == c | a | d
+        else if (b == c)
+            return a * d;
+        // b == d | a | c
+        else if (b == d)
+            return a * c;
+        // c == d | a | b
+        else if (c == d)
+            return a * b;
+        // a | b | c | d
+        else
+            return Math.min(a, Math.min(b, Math.min(c, d)));
     }
 }
