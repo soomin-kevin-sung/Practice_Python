1a456efca3cb2bf9ae57522435e7f76633970f5d
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-08 10:42:41 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.19 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -4,22 +4,21 @@ class Solution {
     public int solution(int[] array) {
         Map<Integer, Integer> map = new HashMap<>();
         
-        int answer = 0, max = 0, cnt = 0;
+        int answer = 0, max = 0;
         for (int i = 0; i < array.length; i++) {
             int value = map.getOrDefault(array[i], 0) + 1;
             if (max < value)
             {
                 answer = array[i];
                 max = value;
-                cnt = 1;
             }
             else if (max == value) {
-                cnt++;
+                answer = -1;
             }
                 
             map.put(array[i], value);
         }
         
-        return cnt > 1 ? -1 : answer;
+        return answer;
     }
 }
