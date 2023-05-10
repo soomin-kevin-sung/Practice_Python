b31f380ae8672989a1718edb4cd77b2705c84eb9
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-10 14:32:38 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.31 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,33 +1,67 @@
+import java.util.*;
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {        
-        int moving_weight = truck_weights[0];
-        int head = 0, tail = 0, cnt = 0;
-        int[] time = new int[truck_weights.length];
+        TruckQueue q = new TruckQueue(bridge_length, weight, truck_weights);
+        int time = 0;
         
-        moving_weight = truck_weights[tail];
-        time[tail] = cnt;
-        tail++;
-        cnt++;
+        q.push(time);
+        time++;
         
-        while (head < tail) {
-            if (head < time.length &&
-                cnt - time[head] >= bridge_length) {
-                moving_weight -= truck_weights[head];
-                head++;
-            }
+        while (q.isTruckLeft()) {
+            if (q.canPop(time))
+                q.pop();
             
-            if (tail < truck_weights.length &&
-                tail - head < bridge_length && 
-                moving_weight + truck_weights[tail] <= weight)
-            {
-                moving_weight += truck_weights[tail];
-                time[tail] = cnt;
-                tail++;
-            }
+            if (q.canPush())
+                q.push(time);
             
-            cnt++;
+            time++;
         }
         
-        return cnt;
+        return time;
+    }
+    
+    private class TruckQueue {   
+        public TruckQueue(int bridge_length, int weight, int[] truck_weights) {
+            this.bridge_length = bridge_length;
+            this.weight = weight;
+            this.truck_weights = truck_weights;
+            this.time = new int[truck_weights.length];
+        }
+        
+        int bridge_length;
+        int weight;
+        int[] truck_weights;
+        int[] time;
+        int head;
+        int tail;
+        int moving_weight;
+        int date;
+        
+        public boolean isTruckLeft() {
+            return head < tail;
+        }
+        
+        public boolean canPush() {
+            return tail < truck_weights.length &&
+                tail - head < bridge_length && 
+                moving_weight + truck_weights[tail] <= weight;
+        }
+        
+        public void push(int t) {
+            moving_weight += truck_weights[tail];
+            time[tail] = t;
+            tail++;
+        }
+        
+        public boolean canPop(int t) {
+            return head < time.length &&
+                t - time[head] >= bridge_length;
+        }
+        
+        public void pop() {
+            moving_weight -= truck_weights[head];
+            head++;
+        }
     }
 }
