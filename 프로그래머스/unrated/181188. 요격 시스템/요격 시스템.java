a884df3f425e9b7c6e62fe349200182437917999
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-05-13 23:12:42 +0900

    [unrated] Title: 요격 시스템, Time: 0.47 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -3,9 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         Arrays.sort(targets, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
-        
+
         int answer = 0, s = targets[0][0], e = targets[0][1];
-        
+
         for (int i = 1; i < targets.length; i++) {
             if (targets[i][0] < e) {
                 e = Math.min(e, targets[i][1]);
@@ -16,7 +16,7 @@ class Solution {
             }
             s = targets[i][0];
         }
-        
+
         return answer + 1;
     }
 }
