d8547fefd9e229a4d10bcddaa2fc3e45099e4533
Soomin(Kevin) Sung <ssm0725@gmail.com>
2023-08-01 15:56:51 +0900

    [Silver II] Title: 부분수열의 합, Time: 276 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -4,25 +4,19 @@ input = sys.stdin.readline
 print = sys.stdout.write
 
 
-def dfs(v, start):
+def dfs(v, i):
     c = 0
 
-    if v == s and start > 0:
-        c = 1
-
-    for i in range(start, n):
-        if used[i]:
-            continue
-
-        used[i] = True
+    if i < n:
+        c += dfs(v, i + 1)
         c += dfs(v + seq[i], i + 1)
-        used[i] = False
+        if v + seq[i] == s:
+            c += 1
 
     return c
 
 
 n, s = map(int, input().split())
 seq = list(map(int, input().split()))
-used = [False for _ in range(n)]
 
 print(f'{dfs(0, 0)}')
